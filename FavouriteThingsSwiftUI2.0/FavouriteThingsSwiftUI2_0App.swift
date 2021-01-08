ccab7c031a3f7f9ef26691f329cbfcd3d42992b2
James Wu <ywu@lcs.on.ca>
2021-01-08 14:44:38 -0500

    Initial Commit

---
+++ FavouriteThingsSwiftUI2.0/FavouriteThingsSwiftUI2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  FavouriteThingsSwiftUI2_0App.swift
+//  FavouriteThingsSwiftUI2.0
+//
+//  Created by James Wu on 2021-01-08.
+//
+
+import SwiftUI
+
+@main
+struct FavouriteThingsSwiftUI2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
